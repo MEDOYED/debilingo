bc805094f14a3eaa9a141ed1d4ebca31bb97e142
MEDOYED <mmax26072005@gmail.com>
2025-08-20 21:28:41 +0200

    feat(words.ts): filled in 11 new words

--- constants/words.ts
+++ constants/words.ts
@@ -19,4 +19,48 @@ export const words = [
     en: "digit",
     uk: "цифра",
   },
+  {
+    en: "note",
+    uk: "примітка",
+  },
+  {
+    en: "measure",
+    uk: "виміряти",
+  },
+  {
+    en: "measurement",
+    uk: "вимір",
+  },
+  {
+    en: "unlike",
+    uk: "на відміру від",
+  },
+  {
+    en: "axis",
+    uk: "вісь",
+  },
+  {
+    en: "enhanced",
+    uk: "розширений",
+  },
+  {
+    en: "directly",
+    uk: "безпосередньо",
+  },
+  {
+    en: "delay",
+    uk: "затримка",
+  },
+  {
+    en: "appropriate",
+    uk: "відповідний",
+  },
+  {
+    en: "sequence",
+    uk: "послідовність",
+  },
+  {
+    en: "interrupt",
+    uk: "перебивати",
+  },
 ];
