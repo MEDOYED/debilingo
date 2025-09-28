233301224a52675449497e954996fc41a0c38ec5
MEDOYED <mmax26072005@gmail.com>
2025-09-28 19:56:05 +0200

    feat(mobile-navigation and dictionaty-add icon): basic html structure and add icon

--- web-app/src/shared/ui/icons/index.ts
+++ web-app/src/shared/ui/icons/index.ts
@@ -1,1 +1,2 @@
 export { ArrowLongRight } from "./arrow-long-right";
+export { DictionaryAdd } from "./dictionary-add";
--- web-app/src/widgets/layout/navigation/ui/mobile-navigation/mobile-navigation.tsx
+++ web-app/src/widgets/layout/navigation/ui/mobile-navigation/mobile-navigation.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
+import { DictionaryAdd } from "@/shared/ui/icons";
+
 const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+  return (
+    <nav aria-label="Головна навігація">
+      <ul>
+        <li>
+          <Link to="/">
+            <DictionaryAdd />
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/profile">
+            <DictionaryAdd />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default MobileNavigation;
