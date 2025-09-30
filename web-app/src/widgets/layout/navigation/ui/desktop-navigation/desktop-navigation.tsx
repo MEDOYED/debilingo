87a8adfef8afc1021203e5356df759507278d3d7
Maksym Mokriakov <120312405+MEDOYED@users.noreply.github.com>
2025-09-30 23:40:33 +0000

    feat(desctop-navigation): initial layout

--- web-app/src/widgets/layout/navigation/ui/desktop-navigation/desktop-navigation.tsx
+++ web-app/src/widgets/layout/navigation/ui/desktop-navigation/desktop-navigation.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
+import { ArrowLongRight } from "@/shared/ui/icons";
+
 const DesktopNavigation = () => {
-  return <div>DesktopNavigation</div>;
+  return ( 
+    <nav>
+      <ul>
+        <li>
+          <Link to={"/"}>
+            <ArrowLongRight />
+          </Link>
+        </li>
+
+        <li>
+          <Link to={"/"}>
+            <ArrowLongRight />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
 };
 
 export default DesktopNavigation;
