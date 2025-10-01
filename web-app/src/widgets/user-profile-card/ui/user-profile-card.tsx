08fbe072fe769850e1b20119586e376b7fd8bebe
Maksym Mokriakov <120312405+MEDOYED@users.noreply.github.com>
2025-10-01 19:33:14 +0000

    feat(user-profile-card): improve layout and styles

--- web-app/src/widgets/user-profile-card/ui/user-profile-card.tsx
+++ web-app/src/widgets/user-profile-card/ui/user-profile-card.tsx
@@ -1,12 +1,14 @@
 import profileLogo from "../../../../public/img/profile-logos/1.jpeg"
 
+import s from "./user-profile-card.module.scss"
+
 export const UserProfileCard = () => {
     return(
         <div>
-            <div>
-                <img src={profileLogo} alt="" />
-                <div>
-                    <span>Kuzeluk Andrij</span>
+            <div className={s.profileLogoAndName}>
+                <img className={s.logo} src={profileLogo} alt="" />
+                <div className={s.userInfo}>
+                    <span className={s.firstName}>Kuzeluk Andrij</span>
                     <span>@medoyed</span>
                 </div>
             </div>
