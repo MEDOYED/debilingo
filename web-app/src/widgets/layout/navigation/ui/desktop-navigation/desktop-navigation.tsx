bc172eb016cb99289d1ad959277d67884d4d0170
Maksym Mokriakov <120312405+MEDOYED@users.noreply.github.com>
2025-10-01 15:43:46 +0000

    feat(navigation): add temp styles for desktop-navigation

--- web-app/src/widgets/layout/navigation/ui/desktop-navigation/desktop-navigation.tsx
+++ web-app/src/widgets/layout/navigation/ui/desktop-navigation/desktop-navigation.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 import { ArrowLongRight } from "@/shared/ui/icons";
 
+import s from "./desktop-navigation.module.scss"
+
 const DesktopNavigation = () => {
   return ( 
-    <nav>
+    <nav className={s.navigation}>
       <ul>
         <li>
           <Link to={"/"}>
