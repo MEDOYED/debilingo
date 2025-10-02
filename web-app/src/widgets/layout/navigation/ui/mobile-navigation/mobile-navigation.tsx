2e73e4e8d8bd6a29aa8bfdd146f88a55099e24e1
MEDOYED <mmax26072005@gmail.com>
2025-10-02 09:14:33 +0200

    feat(navigation): layout and styles for mobile-navigations

--- web-app/src/widgets/layout/navigation/ui/mobile-navigation/mobile-navigation.tsx
+++ web-app/src/widgets/layout/navigation/ui/mobile-navigation/mobile-navigation.tsx
@@ -2,17 +2,40 @@ import { Link } from "react-router-dom";
 
 import { ArrowLongRight, DictionaryAdd } from "@/shared/ui/icons";
 
+import s from "./mobile-navigation.module.scss";
+
 const MobileNavigation = () => {
   return (
-    <nav aria-label="Головна навігація">
-      <ul>
-        <li>
+    <nav
+      className={s.navigation}
+      aria-label="Головна навігація"
+    >
+      <ul className={s.list}>
+        <li className={s.listItem}>
           <Link to="/">
             <DictionaryAdd />
           </Link>
         </li>
 
-        <li>
+        <li className={s.listItem}>
+          <Link to="/profile">
+            <ArrowLongRight />
+          </Link>
+        </li>
+
+        <li className={s.listItem}>
+          <Link to="/profile">
+            <ArrowLongRight />
+          </Link>
+        </li>
+
+        <li className={s.listItem}>
+          <Link to="/profile">
+            <ArrowLongRight />
+          </Link>
+        </li>
+
+        <li className={s.listItem}>
           <Link to="/profile">
             <ArrowLongRight />
           </Link>
