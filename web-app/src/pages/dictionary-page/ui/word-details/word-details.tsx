b607923317fda92fb1e8cd45470983894695f54c
Daniil Riazanov <ryazanow45@gmail.com>
2026-05-06 12:42:23 +0200

    style: improve code readability in WordDetails component

--- web-app/src/pages/dictionary-page/ui/word-details/word-details.tsx
+++ web-app/src/pages/dictionary-page/ui/word-details/word-details.tsx
@@ -2,12 +2,12 @@ import type { Word } from "@shared/api/wordApi";
 
 import s from "./word-details.module.scss";
 
-interface IProps {
+interface WordDetailProps {
   className: string;
   word: Word;
 }
 
-export const WordDetails = ({ className, word }: IProps) => {
+export const WordDetails = ({ className, word }: WordDetailProps) => {
   return (
     <div className={className}>
       <div className={s.ulContainer}>
@@ -15,25 +15,45 @@ export const WordDetails = ({ className, word }: IProps) => {
           Додатков{word.translations.length < 3 ? "ий" : "і"} переклад
           {word.translations.length < 3 ? "" : "и"}:
         </p>
+
         <ul className={s.ulClass}>
           {word.translations.slice(1).map((t) => (
-            <li key={t.id}>{t.text}</li>
+            <li
+              className={s.list}
+              key={t.id}
+            >
+              {t.text}
+            </li>
           ))}
         </ul>
       </div>
+
       <div className={s.ulContainer}>
         <p>Пояснення:</p>
+
         <ul className={s.ulClass}>
           {word.definitions.map((def) => (
-            <li key={def.id}>{def.text}</li>
+            <li
+              className={s.list}
+              key={def.id}
+            >
+              {def.text}
+            </li>
           ))}
         </ul>
       </div>
+
       <div className={s.ulContainer}>
         <p>Приклад{word.examples.length < 2 ? "" : "и"}:</p>
+
         <ul className={s.ulClass}>
           {word.examples.map((ex) => (
-            <li key={ex.id}>{ex.text}</li>
+            <li
+              className={s.list}
+              key={ex.id}
+            >
+              {ex.text}
+            </li>
           ))}
         </ul>
       </div>
