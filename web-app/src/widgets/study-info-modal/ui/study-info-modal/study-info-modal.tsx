ff3d632e5f38c8aba202724035e0acf98d101a93
MEDOYED <mmax26072005@gmail.com>
2026-06-02 21:28:26 +0200

    clean(study-info-modal): remove commented code

--- web-app/src/widgets/study-info-modal/ui/study-info-modal/study-info-modal.tsx
+++ web-app/src/widgets/study-info-modal/ui/study-info-modal/study-info-modal.tsx
@@ -1,24 +1,9 @@
-// import { useEffect } from "react";
-
-// import { useProfileStore } from "@entities/profile";
-
-// import { useStudyInfoModalStore } from "../../model/use-study-info-modal-store";
 import { Timer } from "../timer/timer";
 import { XpCounter } from "../xp-counter/xp-counter";
 
 import s from "./study-info-modal.module.scss";
 
 export const StudyInfoModal = () => {
-  // const { updateStudyActivity } = useProfileStore();
-  // const { xpCounter, timeCounter, isReadyToSendData } =
-  //   useStudyInfoModalStore();
-
-  // useEffect(() => {
-  //   if (isReadyToSendData === true) {
-  //     updateStudyActivity(xpCounter, timeCounter);
-  //   }
-  // }, [isReadyToSendData, xpCounter, timeCounter, updateStudyActivity]);
-
   return (
     <div className={s.modal}>
       <Timer />
