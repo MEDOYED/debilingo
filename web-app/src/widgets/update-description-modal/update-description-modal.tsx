8ed32dbfdf7252d54b4b0210fceaa3cc6e950954
MEDOYED <mmax26072005@gmail.com>
2026-06-18 12:36:13 +0200

    rename: app-update-modal -> update-description-modal

---
+++ web-app/src/widgets/update-description-modal/update-description-modal.tsx
@@ -0,0 +1,9 @@
+import s from "./update-description-modal.module.scss";
+
+export const UpdateDescriptionModal = () => {
+  return (
+    <div>
+      <h2 className={s.version}>v 0.2 </h2>
+    </div>
+  );
+};
