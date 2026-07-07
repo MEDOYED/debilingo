538221436e68a62c24f0a165780e3de70843e615
MEDOYED <mmax26072005@gmail.com>
2026-07-08 00:10:21 +0200

    feat(update-description modal): fill new information about new update

--- web-app/src/features/show-update-description/config/updates-data.ts
+++ web-app/src/features/show-update-description/config/updates-data.ts
@@ -13,10 +13,10 @@ export const UPDATES_DATA: UpdatesData[] = [
     version: "0.4",
     date: "31.07.2026",
     manHours: "",
-    features: [
+    features: null,
+    improvements: [
       "Impove modal for creation new word on dictionary page: improve clear and delete unnecessary inputs, add icons for that actions (clear and delete), add padding-right for this buttons",
     ],
-    improvements: ["Version modal window refactor"],
     fixes: null,
     contributors: ["Medoyed"],
   },
