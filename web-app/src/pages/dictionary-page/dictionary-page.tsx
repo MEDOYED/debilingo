6e5faf162003853346ec50e7b9629222ff48c75a
Daniil Riazanov <ryazanow45@gmail.com>
2026-07-18 18:10:45 +0200

    feat: replace audio button with word click and double click when spoiler true

--- web-app/src/pages/dictionary-page/dictionary-page.tsx
+++ web-app/src/pages/dictionary-page/dictionary-page.tsx
@@ -24,7 +24,6 @@ import { SubmitEditWordButton } from "@features/edit-word";
 import { EditableMainTranslationInput } from "@features/edit-word/ui/editable-main-translation-input/editable-main-translation-input";
 import { EditableSourceWordInput } from "@features/edit-word/ui/editable-source-word-input/editable-source-word-input";
 import { ChevronDown } from "@shared/ui/icons";
-import { SpeakerWave } from "@shared/ui/icons/speaker-wave/speaker-wave";
 import s from "./dictionary-page.module.scss";
 
 const LOAD_WORDS = 20;
@@ -155,14 +154,16 @@ export const DictionaryPage = () => {
                         isVisible={isMainLanguageColVisible}
                       >
                         <div className={s.wordAndSpeakerWrapper}>
-                          <span className={s.wordSource}>
+                          <span
+                            onDoubleClick={() => speak(word.source_word)}
+                            onClick={() =>
+                              isMainLanguageColVisible &&
+                              speak(word.source_word)
+                            }
+                            className={s.wordSource}
+                          >
                             {word.source_word}{" "}
                           </span>
-
-                          <SpeakerWave
-                            className={s.speaker}
-                            onClick={() => speak(word.source_word)}
-                          />
                         </div>
                       </Spoiler>
                     )}
